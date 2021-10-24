61350bd32cd342b7a15f218af98f43204c90e185
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2021-10-23 23:00:40 -0700

    First try

--- index.js
+++ index.js
@@ -1,54 +1,260 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 
+//Any lack of tabbing below is simply to preserve formatting in the created README.md file.
+
 inquirer
     .prompt([
         {
             type: "input",
+            name: "name",
+            message: "Your full name:"
+        },
+        {
+            type: "input",
             name: "title",
-            message: "Project Title:",
+            message: "Project title:",
         },
         {
             type: "editor",
             name: "description",
-            message: "Project Description:",
+            message: "Project description:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "editor",
+            name: "installation",
+            message: "Installation instructions:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "editor",
+            name: "usage",
+            message: "Usage instructions:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "editor",
+            name: "contribution",
+            message: "Contribution guidelines:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "editor",
+            name: "tests",
+            message: "Testing instructions:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "list",
+            name: "license",
+            message: "Choose a license:",
+            choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"],
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "input",
+            name: "github",
+            message: "GitHub username:",
         },
         {
-            type: "",
-            name: "",
-            message: "",
+            type: "input",
+            name: "email",
+            message: "E-mail address:",
         },
     ])
     .then((response) => {
+        
+        const fullName = response.name;
+        const currentYear = new Date().getFullYear();
+        const licenseBadge = "";
+        const licenseNotice = "";
+        
+        if (response.license == "GNU AGPLv3") {
 
-    })
+            licenseBadge = "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+            licenseNotice =
+`Copyright (C) ${currentYear}  ${fullName}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
+
+        } else if (response.license == "GNU GPLv3" || response.license == "GNU LGPLv3") {
+            
+            if (response.license == "GNU GPLv3") {
+                licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            } else if (response.license == "GNU LGPLv3") {
+                licenseBadge = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+            }
+            licenseNotice =
+`Copyright (C) ${currentYear}  ${fullName}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.`;
+
+        } else if (response.license == "Mozilla Public License 2.0") {
+            
+            licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+            licenseNotice = `This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.`;
+
+        } else if (response.license == "Apache License 2.0") {
+            
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-yellowgreen.svg)](https://opensource.org/licenses/Apache-2.0)";
+            licenseNotice =
+`Copyright ${currentYear} ${fullName}
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`;
+
+        } else if (response.license == "MIT License") {
+
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            licenseNotice =
+`MIT License
+
+Copyright (c) ${currentYear} ${fullName}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
+            
+        } else if (response.license == "Boost Software License 1.0") {
+
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+            licenseNotice =
+`Boost Software License - Version 1.0 - August 17th, 2003
+
+Permission is hereby granted, free of charge, to any person or organization
+obtaining a copy of the software and accompanying documentation covered by
+this license (the "Software") to use, reproduce, display, distribute,
+execute, and transmit the Software, and to prepare derivative works of the
+Software, and to permit third-parties to whom the Software is furnished to
+do so, all subject to the following:
+
+The copyright notices in the Software and this entire statement, including
+the above license grant, this restriction and the following disclaimer,
+must be included in all copies of the Software, in whole or in part, and
+all derivative works of the Software, unless such copies or derivative
+works are solely in the form of machine-executable object code generated by
+a source language processor.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.`;
+            
+        } else if (response.license == "The Unlicense") {
+
+            licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/License-Unlicense-blue.svg)](http://unlicense.org/)";
+            licenseNotice =
+`This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>`;
+            
+        };
+
+        fs.writeFile("README.md",
+`#${response.title}
+
+##Description
+
+    ${response.description}
+
+${licenseBadge}
+
+##Table of Contents
+
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [Contributing](#Contributing)
+4. [Tests](#Tests)
+5. [License](#License)
+6. [Questions](#Questions)
+
+##Installation
+
+${response.installation}
+
+##Usage
+
+${response.usage}
+
+##Contributing
+
+${response.contribution}
+
+##Tests
+
+${response.tests}
+
+##License
+
+${licenseNotice}
+
+##Questions
+
+If you have questions, contact me on [GitHub](https://github.com/${response.github}) or send me an e-mail at <${response.email}>.`,
+            (err) => err ? console.log(err) : console.log("- README.md created in the root directory -")
+        );
+    });
